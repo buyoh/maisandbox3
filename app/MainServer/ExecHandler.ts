e60d43a79efe3bb5d0fa6f9ce9673835d85a5285
buyoh <japanjetone@live.jp>
2020-07-25 16:43:57 +0900

    simplify kill procedure

--- app/MainServer/ExecHandler.ts
+++ app/MainServer/ExecHandler.ts
@@ -1,7 +1,7 @@
 import CallbackManager from "../lib/launcher/CallbackManager";
 
 type ExecHandlerState = {
-  repost: { [jid: string]: (data: any) => void };
+  postKill: { [jid: string]: (data: any) => void };
 }
 
 export class ExecHandler {
@@ -12,7 +12,7 @@ export class ExecHandler {
 
   constructor(socket_id: string, launcherCallbackManager: CallbackManager) {
     this.socket_id = socket_id;
-    this.socketStorage = { repost: {} };
+    this.socketStorage = { postKill: {} };
     this.launcherCallbackManager = launcherCallbackManager;
   }
 
@@ -32,36 +32,38 @@ export class ExecHandler {
         const caller = this.launcherCallbackManager.multipost(
           (res_data2) => {
             // note: may call this callback twice or more
+            const sid = res_data2.id.sid;
+            if (sid !== this.socket_id) return;  // may not happen
+            res_data2.id = res_data2.id.jid;
+
             if (res_data2.result && res_data2.result.exited) {
               // finish!
-              delete this.socketStorage.repost[jidStr];
+              delete this.socketStorage.postKill[jidStr];
               if (!res_data2.success) {
+                // note: NOT runtime error (it means rejected query)
                 console.error('launcher failed: method=exec: ', res_data2.error);
                 resultEmitter(res_data2);
                 return;
               }
             }
-            const sid = res_data2.id.sid;
-            if (sid !== this.socket_id) return;  // may not happen
-            res_data2.id = res_data2.id.jid;
             resultEmitter(res_data2);
           });
 
         caller.call(null,
           { method: 'exec', cmd: 'ruby', args: ['var/code.rb'], stdin: data.stdin, id: { jid, sid: this.socket_id } }
         );
-        this.socketStorage.repost[jidStr] = caller;
+        this.socketStorage.postKill[jidStr] = () => {
+          caller.call(null,
+            { method: 'kill', id: { jid, sid: this.socket_id } }
+          );
+        };
       })();
 
     }
     else if (data.action == 'kill') {  // TODO: 実装が汚すぎる
-
-      const caller = this.socketStorage.repost[jidStr];
+      const caller = this.socketStorage.postKill[jidStr];
       if (!caller) return;  // do nothing if jid is unknown
-      caller.call(null,
-        { method: 'kill', id: { jid, sid: this.socket_id } }
-      );
-
+      caller.call(null);
     }
   }
 }
