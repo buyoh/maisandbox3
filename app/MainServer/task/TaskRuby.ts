148b6f360f39688381cfa284b3a40b072d197595
buyoh <japanjetone@live.jp>
2020-07-25 18:18:14 +0900

    simplify task

--- app/MainServer/task/TaskRuby.ts
+++ app/MainServer/task/TaskRuby.ts
@@ -1,5 +1,5 @@
 import CallbackManager from "../../lib/launcher/CallbackManager";
-import { ResultEmitter, Runnable } from "./TaskUtil";
+import { asyncError, ResultEmitter, Runnable } from "./TaskUtil";
 
 export class TaskRuby {
 
@@ -7,16 +7,14 @@ export class TaskRuby {
   private launcherCallbackManager: CallbackManager;
   private resultEmitter: ResultEmitter;
   private finalize: Runnable;
-  private handleKill: Runnable;
+  private handleKill?: Runnable;
 
   constructor(socketId: string, launcherCallbackManager: CallbackManager, resultEmitter: ResultEmitter, finalize: Runnable) {
     this.socketId = socketId;
     this.launcherCallbackManager = launcherCallbackManager;
     this.resultEmitter = resultEmitter;
     this.finalize = finalize;
-
     this.handleKill = null;
-
     this.kill = this.kill.bind(this);
   }
 
@@ -24,44 +22,54 @@ export class TaskRuby {
     this.handleKill?.call(this);
   }
 
-  async startAsync(data: any, jid: any) {
-
+  private async phase1(data: any, jid: any) {
     const res_data1 = await this.launcherCallbackManager.postp(
       { method: 'store', files: [{ path: 'var/code.rb', data: data.code }] });
-
     if (!res_data1.success) {
-      console.error('launcher failed: method=store: ', res_data1.error);
       this.resultEmitter(res_data1);
-      return;
+      return await asyncError('launcher failed: method=store: ' + res_data1.error);
     }
-    const caller = this.launcherCallbackManager.multipost(
-      (res_data2) => {
-        // note: may call this callback twice or more
-        const sid = res_data2.id.sid;
-        if (sid !== this.socketId) return;  // may not happen
-        res_data2.id = res_data2.id.jid;
+  }
 
-        if (res_data2.result && res_data2.result.exited) {
-          // finish!
-          // delete this.socketStorage.postKill[jidStr];
-          this.finalize();
-          if (!res_data2.success) {
-            // note: NOT runtime error (it means rejected query)
-            console.error('launcher failed: method=exec: ', res_data2.error);
-            this.resultEmitter(res_data2);
-            return;
+  private phase2(data: any, jid: any) {
+    return new Promise((resolve, reject) => {
+      const caller = this.launcherCallbackManager.multipost(
+        (res_data2) => {
+          // note: may call this callback twice or more
+          res_data2.id = res_data2.id.jid;
+          if (res_data2.result && res_data2.result.exited) {
+            this.finalize();
+            this.handleKill = null;
+            if (res_data2.success) {
+              resolve();
+            } else {
+              // note: NOT runtime error (it means rejected a bad query)
+              console.error('launcher failed: method=exec: ', res_data2.error);
+              reject();
+            }
           }
-        }
-        this.resultEmitter(res_data2);
-      });
-
-    caller.call(null,
-      { method: 'exec', cmd: 'ruby', args: ['var/code.rb'], stdin: data.stdin, id: { jid, sid: this.socketId } }
-    );
-    this.handleKill = () => {
+          this.resultEmitter(res_data2);
+        });
       caller.call(null,
-        { method: 'kill', id: { jid, sid: this.socketId } }
+        { method: 'exec', cmd: 'ruby', args: ['var/code.rb'], stdin: data.stdin, id: { jid, sid: this.socketId } }
       );
-    };
+      this.handleKill = () => {
+        caller.call(null,
+          { method: 'kill', id: { jid, sid: this.socketId } }
+        );
+      };
+    });
+  }
+
+  async startAsync(data: any, jid: any) {
+    console.log('task start:' + jid);
+    try {
+      await this.phase1(data, jid);
+      await this.phase2(data, jid);
+    } catch (e) {
+      console.error(e);
+      this.finalize();
+    }
+    console.log('task complete:' + jid);
   }
 }
--- app/MainServer/task/TaskUtil.ts
+++ app/MainServer/task/TaskUtil.ts
@@ -4,3 +4,7 @@ export type ResultEmitter = (data: any) => void;
 export function getWorkDirectory() {
   return 'var';
 }
+
+export function asyncError(message) {
+  return Promise.reject(new Error(message));
+}
