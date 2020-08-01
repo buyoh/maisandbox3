2ed787cfb7102a8175d05796b851aee86fa6420d
buyoh <japanjetone@live.jp>
2020-08-02 00:51:00 +0900

    avoid job_id_str collision when open some tabs

--- launcher/AppLauncher/ALReciever.rb
+++ launcher/AppLauncher/ALReciever.rb
@@ -41,10 +41,14 @@ class ALReciever
       # json.idを失わないようにALRecieverで管理する
       # ALRecieverの重要な役割のひとつ
       id = json_line['id'] # task-unique
-      vlog id
+      job_id = json_line['id']['jid'] # (client)ビルド→実行のワークフローで共通　KILLも共通
+      socket_id = json_line['id']['sid'] # (server)ページ単位で共通
+      lcm_id =  json_line['id']['lcmid'] # (server)ユーザのアクション単位で共通　KILLは別のアクションなのでlcmidは異なる(launcher callback id)
+
       id_str = JSON.generate(id).hash.to_s(36)
-      job_id = json_line['id']['jid'] # task_list-unique
-      job_id_str = JSON.generate(job_id).hash.to_s(36)
+      # note: job_idは純粋な連番なので、複数ページを同時に開くだけで衝突する
+      # socket_idを結合してこれを回避する
+      job_id_str = (JSON.generate(job_id) + '_' + JSON.generate(socket_id)).hash.to_s(36)
       # socket_id = json_line['id']['sid'] # socket-user-unique
       # TODO: 削除しないと貯まる まじで
       ls = @local_storage_manager[job_id_str]
