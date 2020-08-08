f0d309558964fe848cd3ecb2e339f9ac3b2b36f2
buyoh <japanjetone@live.jp>
2020-08-08 18:11:10 +0900

    get_boxdir return absolute path

--- spec/test/ALDirectoryManager_spec.rb
+++ spec/test/ALDirectoryManager_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe ALDirectoryManager do
     dm.install_user user1
     user1_box1 = dm.new_box(user1)
     user1_box1_dir = dm.get_boxdir(user1, user1_box1)
-    expect(Dir.exist?(ALBase.work_directory + user1_box1_dir)).to eq true
+    expect(Dir.exist?(user1_box1_dir)).to eq true
     user1_box2 = dm.new_box(user1)
     user1_box2_dir = dm.get_boxdir(user1, user1_box2)
-    expect(Dir.exist?(ALBase.work_directory + user1_box2_dir)).to eq true
+    expect(Dir.exist?(user1_box2_dir)).to eq true
 
     expect(dm.user_exists?(user1)).to eq true
     expect(dm.box_exists?(user1, user1_box1)).to eq true
@@ -23,17 +23,17 @@ RSpec.describe ALDirectoryManager do
     dm.install_user user2
     user2_box1 = dm.new_box(user2)
     user2_box1_dir = dm.get_boxdir(user2, user2_box1)
-    expect(Dir.exist?(ALBase.work_directory + user2_box1_dir)).to eq true
+    expect(Dir.exist?(user2_box1_dir)).to eq true
     user2_box2 = dm.new_box(user2)
     user2_box2_dir = dm.get_boxdir(user2, user2_box2)
-    expect(Dir.exist?(ALBase.work_directory + user2_box2_dir)).to eq true
+    expect(Dir.exist?(user2_box2_dir)).to eq true
 
     expect(user1_box1_dir).not_to eq user1_box2_dir
     expect(user1_box1_dir).not_to eq user2_box1_dir
 
     dm.uninstall_user user1
-    expect(Dir.exist?(ALBase.work_directory + user1_box1_dir)).to eq false
-    expect(Dir.exist?(ALBase.work_directory + user2_box1_dir)).to eq true
+    expect(Dir.exist?(user1_box1_dir)).to eq false
+    expect(Dir.exist?(user2_box1_dir)).to eq true
     dm.uninstall_user user2
   end
 end
