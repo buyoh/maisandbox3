676d0a09d6879a8fedf212246ea4cf57ec779015
buyoh <15198247+buyoh@users.noreply.github.com>
2024-02-19 09:33:13 +0900

    Refactoring main

--- src/app/MainServer.ts
+++ src/app/MainServer.ts
@@ -1,68 +1,18 @@
-import Next from 'next';
-
-import Config from '../lib/Config';
-import { setupExpressServer } from './MainServer/Express';
-import {
-  setupSocketIO,
-  setupSocketIOAndBindHandler,
-} from './MainServer/SocketIO';
-import LauncherHolder from './Launcher/LauncherHolder';
-import { createLauncherSocket } from './Launcher/LauncherSocketFactory';
-import { readFileSync } from 'fs';
-
-const appNext = Next({ dev: Config.develop });
-const port = Config.httpPort;
+import { createLauncherService } from './LauncherService';
+import { createWebService } from './WebService';
+import { createTaskManagerService } from './TaskManagerService';
 
 (async () => {
   try {
-    // launcher
-    const launcherHolder = new LauncherHolder(
-      5000,
-      (code: number | null) => {
-        if (Config.useChildProcess) {
-          if (code === 1) {
-            console.error('launcher has raised exceptions');
-            process.exit(1);
-          } else {
-            console.error('launcher disconnected code=' + code);
-            process.exit(0);
-          }
-          return false;
-        }
-        return true;
-      },
-      () => createLauncherSocket()
+    const launcherService = createLauncherService();
+    const taskManagerService = createTaskManagerService(
+      launcherService.getCallbackManager()
     );
-    launcherHolder.start();
-
-    // sslconfig
-    const sslConfigPath = Config.sslConfigPath;
-    const sslConfig = !sslConfigPath
-      ? null
-      : Object.entries(
-          JSON.parse(readFileSync(sslConfigPath).toString()) as {
-            [index: string]: string;
-          }
-        ).reduce((s, keyPath) => {
-          s[keyPath[0]] = readFileSync(keyPath[1]).toString();
-          return s;
-        }, {} as { [index: string]: string });
-
-    // appServer
-    await appNext.prepare();
-    const [, httpServer] = setupExpressServer(
-      appNext.getRequestHandler(),
-      port,
-      sslConfig
-    );
-
-    // socketio
-    const socketio = setupSocketIO(httpServer);
-    setupSocketIOAndBindHandler(socketio, launcherHolder.callbackManager);
+    await createWebService(taskManagerService.getConnectionHandlerFactory());
 
     // trap
     process.on('SIGINT', () => {
-      launcherHolder.stop();
+      launcherService.stop();
       process.exit(0);
     });
   } catch (e) {
