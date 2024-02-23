2339bd6dbb31800bcefe3cc410d808df278a82a0
buyoh <15198247+buyoh@users.noreply.github.com>
2024-02-24 08:34:18 +0900

    remove nextjs

--- src/web/components/style/meta.tsx
+++ src/web/components/style/meta.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+/*
+// Next
 import Head from 'next/head';
+*/
 
-// TODO: Only used for nextjs, remove for vite
 export default function Meta(): JSX.Element {
+  // Nextjs is no longer supported.
+  return <></>;
+  /*
   return (
     <Head>
       <title>maisandbox</title>
       <link rel="icon" href="/icons/787_me_f.svg" />
     </Head>
   );
+  */
 }
 Meta.displayName = 'Meta';
